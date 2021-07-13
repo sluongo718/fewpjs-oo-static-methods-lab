168b7e4b25dcbce3b6a2a50e1ac7e1aa62aee0f5
sluongo718 <sluongo718@yahoo.com>
2021-07-13 00:51:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-z0-9- \.',_]/gim,"");
+  }
+
+  static titleize(sentence) {
+    const newSentence = []
+    sentence.split(" ").forEach(word => {
+      switch (word) {
+        case 'the':
+          newSentence.push(word);
+          break;
+        case 'a':
+          if (sentence[0] === 'a'){
+            newSentence.push(word.toUpperCase());
+          } else {
+            newSentence.push(word);
+          }
+          break;
+        case 'an':
+          newSentence.push(word);
+          break;
+        case 'but':
+          newSentence.push(word);
+          break;
+        case 'of':
+          newSentence.push(word);
+          break;
+        case 'and':
+          newSentence.push(word);
+          break;
+        case 'for':
+          newSentence.push(word);
+          break;
+        case 'at':
+          newSentence.push(word);
+          break;
+        case 'by':
+          newSentence.push(word);
+          break;
+        case 'from': 
+          newSentence.push(word);
+          break;
+        default:
+          newSentence.push(word.charAt(0).toUpperCase() + word.slice(1));
+          break;
+      }
+    });
+    return newSentence.join(" ");
+  }
+  }
+
